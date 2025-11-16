6c64e8995e23c656e97ee1db0389d1b81e779308
j-vercammen <crowbar.moots-8u@icloud.com>
2025-11-16 08:03:33 +0000

    After chapter 3

--- chat.py
+++ chat.py
@@ -1,11 +1,34 @@
 from ctransformers import AutoModelForCausalLM
 
-llm = AutoModelForCausalLM.from_pretrained("zoltanctoth/orca_mini_3B-GGUF", model_file="orca-mini-3b.q4_0.gguf")
+# llm = AutoModelForCausalLM.from_pretrained("zoltanctoth/orca_mini_3B-GGUF", model_file="orca-mini-3b.q4_0.gguf")
+llm = AutoModelForCausalLM.from_pretrained("TheBloke/Llama-2-7B-Chat-GGUF", model_file="llama-2-7b-chat.Q5_K_M.gguf")
 
+"""
+def get_prompt(instruction: str) -> str:
+    system = "You are an Al assistant that gives helpful answers.  You answer the questions in a short and concise way."
+    prompt = f"### System:\n{system}\n\n### User:\n{instruction}\n\n### Response:\n"
+    return prompt
+"""
+
+
+def get_prompt(instruction: str) -> str:
+    system = "You are an Al assistant that gives helpful answers.  You answer the questions in a short and concise way."
+    prompt = f"<s>[INST] <<SYS>>\n{system}\n<</SYS>>\n\n{instruction} [/INST]"
+    return prompt
+
+
+question = "Which is the capital city of India?"
+
+for word in llm(get_prompt(question), stream=True):
+    print(word, end="", flush=True)
+print()
+
+
+"""
 prompt = "Hi! The name of the capital city of India is"
 prompt2 = "Hi! What is the name of the capital city of India?"
-
 
 for word in llm(prompt, stream=True):
     print(word, end="", flush=True)
 print()
+"""
