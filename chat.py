55964d2b6ce25f190f0a345aa30fbc07f87a7f1b
j-vercammen <crowbar.moots-8u@icloud.com>
2025-11-16 14:25:09 +0000

    After chapter 4

--- chat.py
+++ chat.py
@@ -1,25 +1,41 @@
+from typing import List
 from ctransformers import AutoModelForCausalLM
 
-# llm = AutoModelForCausalLM.from_pretrained("zoltanctoth/orca_mini_3B-GGUF", model_file="orca-mini-3b.q4_0.gguf")
-llm = AutoModelForCausalLM.from_pretrained("TheBloke/Llama-2-7B-Chat-GGUF", model_file="llama-2-7b-chat.Q5_K_M.gguf")
+llm = AutoModelForCausalLM.from_pretrained("zoltanctoth/orca_mini_3B-GGUF", model_file="orca-mini-3b.q4_0.gguf")
+# llm = AutoModelForCausalLM.from_pretrained("TheBloke/Llama-2-7B-Chat-GGUF", model_file="llama-2-7b-chat.Q5_K_M.gguf")
 
-"""
-def get_prompt(instruction: str) -> str:
+
+def get_prompt(instruction: str, history: List[str] = None) -> str:
     system = "You are an Al assistant that gives helpful answers.  You answer the questions in a short and concise way."
-    prompt = f"### System:\n{system}\n\n### User:\n{instruction}\n\n### Response:\n"
+    prompt = f"### System:\n{system}\n\n### User:\n"
+    if history is not None:
+        prompt += f"This is the conversation history: {''. join(history)}. Now answer the question: "
+    prompt += f"{instruction}\n\n### Response:\n"
+    print(prompt)
     return prompt
+
+
 """
-
-
 def get_prompt(instruction: str) -> str:
     system = "You are an Al assistant that gives helpful answers.  You answer the questions in a short and concise way."
     prompt = f"<s>[INST] <<SYS>>\n{system}\n<</SYS>>\n\n{instruction} [/INST]"
     return prompt
+"""
 
-
+history = []
 question = "Which is the capital city of India?"
+answer = ""
+
 
 for word in llm(get_prompt(question), stream=True):
+    print(word, end="", flush=True)
+    answer += word
+print()
+history.append(answer)
+
+question = "And which is of the United States?"
+
+for word in llm(get_prompt(question, history), stream=True):
     print(word, end="", flush=True)
 print()
 
