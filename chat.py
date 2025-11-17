0a76313041343250fd81379f826be056164874d6
j-vercammen <crowbar.moots-8u@icloud.com>
2025-11-17 07:57:01 +0000

    After chapter 5

--- chat.py
+++ chat.py
@@ -1,18 +1,41 @@
+import chainlit as cl
 from typing import List
 from ctransformers import AutoModelForCausalLM
 
-llm = AutoModelForCausalLM.from_pretrained("zoltanctoth/orca_mini_3B-GGUF", model_file="orca-mini-3b.q4_0.gguf")
 # llm = AutoModelForCausalLM.from_pretrained("TheBloke/Llama-2-7B-Chat-GGUF", model_file="llama-2-7b-chat.Q5_K_M.gguf")
 
 
 def get_prompt(instruction: str, history: List[str] = None) -> str:
     system = "You are an Al assistant that gives helpful answers.  You answer the questions in a short and concise way."
     prompt = f"### System:\n{system}\n\n### User:\n"
-    if history is not None:
+    if len(history) > 0:
         prompt += f"This is the conversation history: {''. join(history)}. Now answer the question: "
     prompt += f"{instruction}\n\n### Response:\n"
     print(prompt)
     return prompt
+
+
+@cl.on_message
+async def on_message(message: cl.Message):
+    message_history = cl.user_session. get("message_history")
+    msg = cl.Message(content="")
+    await msg.send()
+
+    prompt = get_prompt(message.content, message_history)
+    response = ""
+    for word in llm(prompt, stream=True):
+        await msg.stream_token(word)
+        response += word
+    await msg.update()
+    message_history.append(response)
+
+
+@cl.on_chat_start
+def on_chat_start():
+    cl.user_session.set("message_history", [])
+    global llm
+    llm = AutoModelForCausalLM.from_pretrained("zoltanctoth/orca_mini_3B-GGUF", model_file="orca-mini-3b.q4_0.gguf")
+    print("A new chat session has started!")
 
 
 """
@@ -22,6 +45,7 @@ def get_prompt(instruction: str) -> str:
     return prompt
 """
 
+"""
 history = []
 question = "Which is the capital city of India?"
 answer = ""
@@ -38,7 +62,7 @@ question = "And which is of the United States?"
 for word in llm(get_prompt(question, history), stream=True):
     print(word, end="", flush=True)
 print()
-
+"""
 
 """
 prompt = "Hi! The name of the capital city of India is"
